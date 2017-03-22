82ae5adb8c209eb39b9fabed9e0608ea447110f0
pierrejego <pierre.jego@gfi.fr>
2017-03-22 10:55:05 +0100

    Sort towns lit by name in json return
    This fix #18

--- webapps/src/main/java/org/georchestra/photooblique/service/helper/CommuneHelper.java
+++ webapps/src/main/java/org/georchestra/photooblique/service/helper/CommuneHelper.java
@@ -1,9 +1,12 @@
 package org.georchestra.photooblique.service.helper;
 
-
 import java.io.IOException;
 import java.net.MalformedURLException;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.regex.Pattern;
@@ -17,25 +20,23 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-
-
 @Component
-public class CommuneHelper extends ParentHelper{
-	
+public class CommuneHelper extends ParentHelper {
+
 	@Autowired
 	PORepository poRepository;
-	
+
 	final static Logger logger = LoggerFactory.getLogger(CommuneHelper.class);
 
 	public Map<String, Object> getCommunes(List<String> ids, int startPeriod, int endPeriod) {
-		
+
 		Map<String, Object> result = new HashMap<String, Object>();
-		Map<String, Object> communesMap = new HashMap<String, Object>();
-		
+		Map<String, String> communesMap = new HashMap<String, String>();
+
 		Object[] communeArray = null;
-		
+
 		// get Commune code_insee list
-		List<String> codeCommunes  = null;
+		List<String> codeCommunes = null;
 
 		if (startPeriod == 0 && endPeriod == 0) {
 			codeCommunes = poRepository.selectDistinctCommune();
@@ -44,51 +45,68 @@ public class CommuneHelper extends ParentHelper{
 		} else {
 			codeCommunes = poRepository.selectDistinctCommuneByYearBetween(startPeriod, endPeriod);
 		}
-				
+
 		// Add name for each code commune
-		Map<String,String> communes;
+		Map<String, String> communes;
 		try {
 			communes = CommunesList.getInstance().getCommunes();
-		
-			if(communes != null && !communes.isEmpty()){
-				// For each commune found add label if exist, do not add it otherwise
-				for (String code : codeCommunes){
-					
-					if(!StringUtils.isBlank(code)){
+
+			if (communes != null && !communes.isEmpty()) {
+				// For each commune found add label if exist, do not add it
+				// otherwise
+				for (String code : codeCommunes) {
+
+					if (!StringUtils.isBlank(code)) {
 						// if contains |
-						if (code.indexOf('|') != -1){
+						if (code.indexOf('|') != -1) {
 							String[] codes = code.split(Pattern.quote("|"));
 
-							for( String newCode : codes){
+							for (String newCode : codes) {
 								logger.debug("Code commune : " + newCode);
-								if(newCode.length()>4 && communes.get(newCode) != null){
+								if (newCode.length() > 4 && communes.get(newCode) != null) {
 									communesMap.put(newCode, communes.get(newCode));
 								}
 							}
-						}else{
-							if(code.length()>4 && communes.get(code) != null){
+						} else {
+							if (code.length() > 4 && communes.get(code) != null) {
 								communesMap.put(code, communes.get(code));
 							}
 						}
 					}
 				}
 			}
-			
-			// Filter on given commune ids
-			if(!CollectionUtils.isEmpty(ids)){
 
-				Map<String, Object> communesMapTemp = new HashMap<String, Object>();
-				for(String id:ids){
-					if(StringUtils.isNotEmpty(id) && communesMap.get(id) != null){
+			// Filter on given commune ids
+			if (!CollectionUtils.isEmpty(ids)) {
+
+				Map<String, String> communesMapTemp = new HashMap<String, String>();
+				for (String id : ids) {
+					if (StringUtils.isNotEmpty(id) && communesMap.get(id) != null) {
 						logger.debug("Filter on : " + id);
 						communesMapTemp.put(id, communesMap.get(id));
 					}
 				}
-				communesMap = communesMapTemp;			
+				communesMap = communesMapTemp;
+			}
+
+			// Sort commune Map by value
+			List<Map.Entry<String, String>> list = new LinkedList<Map.Entry<String, String>>(communesMap.entrySet());
+			Map<String, String> sortedCommuneMap = new LinkedHashMap<String, String>();
+			
+			Collections.sort(list, new Comparator<Map.Entry<String, String>>() {
+				
+				@Override
+				public int compare(Map.Entry<String, String> entry1, Map.Entry<String, String> entry2) {
+					return (entry1.getValue()).compareToIgnoreCase(entry2.getValue());
+				}
+			});
+			
+			for (Map.Entry<String, String> entry : list) {
+				sortedCommuneMap.put(entry.getKey(), entry.getValue());
 			}
 			
 			// Convert Map to Array to fit with ExtJs LoveCombo Store
-			communeArray = communesMap.entrySet().toArray(); 
+			communeArray = sortedCommuneMap.entrySet().toArray();
 
 			result.put("succes", true);
 			result.put("communes", communeArray);
@@ -98,17 +116,15 @@ public class CommuneHelper extends ParentHelper{
 			logger.error(e.getMessage());
 		} catch (IOException e) {
 			result.put("succes", false);
-			result.put("error",  e.getMessage());
+			result.put("error", e.getMessage());
 			logger.error(e.getMessage());
 		} catch (ParseException e) {
 			result.put("succes", false);
 			result.put("error", e.getMessage());
 			logger.error(e.getMessage());
 		}
-			
+
 		return result;
 	}
-	
-	
-    
+
 }
